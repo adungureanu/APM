c2ebfb34d0b46830415c79a4a6388e596ec0b3ab
adungureanu <ad.ungureanu@yahoo.com>
2017-07-24 15:52:57 +0300

    initial commit

---
+++ example.js
@@ -0,0 +1,15 @@
+var animals = [
+    {name: "Fluff", species:'rabbit'},
+    {name: "Caro", species:'dog'},
+    {name: "Hamilton", species:'dog'},
+    {name: "Harold", species:'fish'},
+    {name: "Ursula", species:'cat'},
+    {name: "Jimmy", species:'fish'}
+]
+
+/*
+var dogs = []
+for(var i = 0; i < animals.length; i++){
+    if(animals[i].species === "dog")
+    dogs.push(abunaks[i])
+}*/
